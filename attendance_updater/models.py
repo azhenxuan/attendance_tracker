9a8c8fe05cffe63c247f93f99f8c11b9a4d91c1a
azhenxuan <a.zhenxuan@gmail.com>
2017-04-14 18:05:39 +0800

    making tables work

--- attendance_updater/models.py
+++ attendance_updater/models.py
@@ -27,7 +27,7 @@ class Attendance(db.Model):
     __tablename__ = 'attendance'
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date)
-    student_nric = db.Column(db.Integer, db.ForeignKey('students.nric'))
+    student_nric = db.Column(db.String(9), db.ForeignKey('students.nric'))
     class_id = db.Column(db.Integer, db.ForeignKey('classes.id'))
     
 class Class(db.Model):
